9003c25c005ebfea5357406f38b35c44a3f1fdde
Fersand07 <00007821@uca.edu.sv>
2023-05-19 12:07:57 -0600

    LABO 09

---
+++ LAB009/app/src/main/java/com/example/lab009/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.lab009
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.corutinesdemo.R
+import com.example.corutinesdemo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //TODO: Use binding to navigate to the other fragments
+        binding.mainThreadButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_mainThreadFragment)
+        }
+
+        binding.coroutinesButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_coroutinesFragment)
+        }
+
+    }
+}
