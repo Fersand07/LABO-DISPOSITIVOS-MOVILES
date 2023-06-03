c6d2b57214693e3ff91ffbfb195778c3325c3fa4
Fersand07 <00007821@uca.edu.sv>
2023-06-02 22:12:53 -0600

    LABO 10

--- LABO 10/app/src/main/java/com/example/laboratorio05/repositories/CastRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.laboratorio05.repositories
-
-import com.example.laboratorio05.data.dao.CastDao
-import com.example.laboratorio05.data.model.ActorModel
-import com.example.laboratorio05.data.model.CastModel
-
-class CastRepository(private val castDao: CastDao) {
-    suspend fun addCasting(casting: CastModel) = castDao.insertCast(casting)
-}
